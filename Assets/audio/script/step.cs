881a050e2271f2e5b5da3c4bf98b3323044648e0
eqzero1 <egor_clab@mail.ru>
2021-01-24 22:54:42 +0300

    123

---
+++ Assets/audio/script/step.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KinematicCharacterController;
+
+public class step : MonoBehaviour
+{
+
+    
+    public string inputsound;
+    bool playerismoving;
+    public float walkingspeed;
+
+
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") >= 0.01f || Input.GetAxis("Horizontal") >= 0.01f || Input.GetAxis("Vertical") <= -0.01f || Input.GetAxis("Horizontal") <= -0.01f)
+        {
+            
+            playerismoving = true;
+        }
+        else if (Input.GetAxis("Vertical") == 0 || Input.GetAxis("Horizontal") == 0)
+        {
+            
+            playerismoving = false;
+        }
+
+    }
+
+    void CallFootsteps()
+    {
+        if (playerismoving == true)
+        { 
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Sound FX/soudperson/steps1");
+        }
+    }
+
+
+    void Start()
+    {
+        InvokeRepeating("CallFootsteps", 0, walkingspeed);
+    }
+
+
+    void OnDisable()
+    {
+        playerismoving = false;
+    }
+}
